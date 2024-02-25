de3f9f015de1918cb74e5c23bc9f2dfa129019db
Dario Pettinella <dario.pettinella@gmail.com>
2024-02-25 13:01:51 +0000

    Created CMake for AlsaBufferConverter. Linked to main.cpp. Added visual studio to gitignore.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 
 int main()
 {
-    const size_t numOfValues = 5;
+    const size_t numOfHexValues = 5;
+    const size_t numOfBytes = 3 * numOfHexValues;
 
-    uint8_t buf[numOfValues][3] = {{0x7F, 0xFF, 0xFF},{0x40, 0x00, 0x00},{0x00, 0x00, 0x00},{0xc0, 0x00, 0x00},{0x80, 0x00, 0x00}};
+    //uint8_t buf[numOfHexValues][3] = {{0x7F, 0xFF, 0xFF},{0x40, 0x00, 0x00},{0x00, 0x00, 0x00},{0xc0, 0x00, 0x00},{0x80, 0x00, 0x00}};
+    uint8_t buf[numOfBytes] = { 0x7F, 0xFF, 0xFF, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,0xc0, 0x00, 0x00, 0x80, 0x00, 0x00 };
 
     int32_t val;
 
     AlsaBufferConverter test;
 
-    for (int index = 0; index < numOfValues; index++)
+    for (int index = 0; index < numOfBytes; index+=3)
     {
-        val = test.getInt32FromBuffer(buf[index]);
+        val = test.getInt32FromBuffer(buf+index);
 
         std::cout << std::dec << val << std::endl;
         std::cout << std::hex << val << "\n" << std::endl;
