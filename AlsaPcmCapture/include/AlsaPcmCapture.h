a52353230a695c9bbd14b08f16e07913aadba56f
Jed Scott <jedscott50@gmail.com>
2024-02-29 09:56:18 +0000

    created setting struct for pcm capture

--- AlsaPcmCapture/include/AlsaPcmCapture.h
+++ AlsaPcmCapture/include/AlsaPcmCapture.h
@@ -2,12 +2,61 @@
 
 #ifndef ALSAPCMCAPTURE_H
 #define ALSAPCMCAPTURE_H
-class PcmCapture:
+
+
+
+class PcmCapture
 {
-public:
+	public:
+		void startI2S(){}
+		
+			
+	
+	
+
+	private:
+	
+		struct PcmAudioSettings {
+			
+			
+			/* create device handle object */
+			snd_pcm_t *handle; 
+			
+			/* device name (default pcm device) */
+			const char *device_name;
+			
+			
+			
+			
+			/* create params object */
+			snd_pcm_hw_params_t *params;
+			
+			/* number of audio frames stored in buffer (period size))*/
+			snd_pcm_uframes_t frames;
+			
+			/* audio format mode */
+			snd_pcm_format_t format;
+			
+			/* sample rate */
+			unsigned int rate;
+			
+			/* number of channels */
+			unsigned int nchannels;
+			
+			
+			/* buffer that digital audio will  be written to after capture */
+			char *buffer;
+			
+			/* buffer size should be enough to contain one period */
+			int buffer_size;
+
+			
+			};
+	
+	
+	
 
 
-private:
 };
 
 #endif
