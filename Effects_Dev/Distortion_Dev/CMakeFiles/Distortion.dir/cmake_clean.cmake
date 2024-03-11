116caf17e8dd7630b9a5e465777765b8f04e32a4
Elad <eladp2001@gmail.com>
2024-03-11 16:49:57 +0000

    Move file into distortion_dev directory

---
+++ Effects_Dev/Distortion_Dev/CMakeFiles/Distortion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Distortion.dir/Distortion.cpp.o"
+  "CMakeFiles/Distortion.dir/Distortion.cpp.o.d"
+  "Distortion"
+  "Distortion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Distortion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
