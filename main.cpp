edd44810c0f38a51bfce5af415568abb225ef313
Dario Pettinella <dario.pettinella@gmail.com>
2024-03-25 11:43:12 +0000

    Renamed main directory to src. Added additional conversion tests. Added ability to change device names in program config in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <alsa/asoundlib.h>
 #include <iostream>
-#include "AlsaEffects/include/EffectsManager.h"
-#include "AlsaEffects/include/PcmAudioCapture.h"
-#include "AlsaEffects/include/PcmAudioPlayback.h"
-#include "AlsaEffects/include/EffectBase.h"
+#include "src/include/EffectsManager.h"
+#include "src/include/PcmAudioCapture.h"
+#include "src/include/PcmAudioPlayback.h"
+#include "src/include/EffectBase.h"
 
 int main()
 {
     //////////// Program Configuration START ////////////
+
+    // Declare audio device names
+    const char* captureDevice = "hw:1,1";
+    const char* playbackDevice = "default";
 
     // Optimum Value = 9600;
     snd_pcm_uframes_t framesPerBuffer = 9600;
@@ -50,9 +54,9 @@ int main()
 
     EffectsManager* em = new EffectsManager(endian, bytesPerSample, framesPerBuffer);
 
-    PcmAudioPlayback* play = new PcmAudioPlayback("default", format, framesPerBuffer, sampleRate);
+    PcmAudioPlayback* play = new PcmAudioPlayback(playbackDevice, format, framesPerBuffer, sampleRate);
 
-    PcmAudioCapture* cap = new PcmAudioCapture("hw:1,1", format, framesPerBuffer, sampleRate);
+    PcmAudioCapture* cap = new PcmAudioCapture(captureDevice, format, framesPerBuffer, sampleRate);
 
     cap->registerCallback(em);
     em->registerCallback(play);
